c76599197f8b3a52869d88e4ef4050ce9030ad37
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-28 09:12:09 -0400

    parties

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String party;
+        System.out.println("Please enter a letter for your party. R for Republican. D for Democrat. I for Independent");
+        party = scan.nextLine();
+        if (party.equals("R")) {
+            System.out.println("You get a Republican Elephant");
+        }
+        else if (party.equals("D")) {
+            System.out.println("you get a Democrat Donkey");
+        } else if (party.equals("I")) {
+            System.out.println("You get Independent Person");
+        }
+        else {
+            System.out.println("You have entered an invalid string");
+            System.exit(0);
+        }
+
+
     }
 }
